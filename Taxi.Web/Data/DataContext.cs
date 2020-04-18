c169b47859a34ee39dfb08c033c3987156017831
Alonso Ugalde <augaldecr@hotmail.com>
2020-04-18 00:41:10 -0600

    Added Entities

---
+++ Taxi.Web/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Taxi.Web.Data.Entities;
+
+namespace Taxi.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<TaxiEntity> Taxis { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<TripDetail> TripDetails { get; set; }
+    }
+}
---
+++ Taxi.Web/Data/Entities/TaxiEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Taxi.Web.Data.Entities
+{
+    public class TaxiEntity
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [RegularExpression(@"^([A-Aa-a]{3}\d{3})$", ErrorMessage = "The field {0} must have a valid format")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "The field {0} must have more than {1} characters")]
+        public string Plaque { get; set; }
+
+        public virtual ICollection<Trip> Trips { get; set; }
+    }
+}
